2dc6617ef58eb8afdeee426643a33763740868bd
zhangxin <XZhang1@cccis.com>
2019-03-14 13:42:01 +0800

    hide key info may be inLegal

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 const fs=require('fs');
 const path=require('path');
 
-const rootPath=path.normalize("F:\\daily\\apd_encount\\my_doc\\apd3.7\\规则1.5VS规则1.3.3\\1.3");
-// const rootPath=path.normalize("F:\\hardy\\svn-local\\apd-apd-git\\apd-web");
+const rootPath=path.normalize("");
+
 fs.stat(rootPath,function(err,stats){
     console.log(stats);
 });
